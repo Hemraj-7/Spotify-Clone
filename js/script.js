bae5a6c8af91bfc74a75f8c163f8f153c5ffbc34
Hemraj-7 <mhemraj814@gmail.com>
2024-06-05 23:53:37 +0530

    First Commit Spotify clone Adding on github

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currentFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
+    let a = await fetch(`./${folder}/`);
     let responce = await a.text();
     let div = document.createElement("div")
     div.innerHTML = responce;
@@ -75,7 +75,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`./songs/`)
     let responce = await a.text();
     let div = document.createElement("div")
     div.innerHTML = responce;
@@ -88,7 +88,7 @@ async function displayAlbums() {
         if (e.href.includes(`/songs`)) {
             let folder = e.href.split("/").slice(-2)[0]
             // get the meta data of the folder
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`./songs/${folder}/info.json`)
             let responce = await a.json();
             console.log(responce);
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card rounded">
